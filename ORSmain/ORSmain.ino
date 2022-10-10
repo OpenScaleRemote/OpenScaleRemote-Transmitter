c55e1a903a2ad1921fc171c478a8de756481bc9d
DerGregHD <55300769+DerGregHD@users.noreply.github.com>
2022-10-10 16:15:51 +0200

    Update ORSmain.ino

--- ORSmain/ORSmain.ino
+++ ORSmain/ORSmain.ino
@@ -10,6 +10,7 @@
 
 //########## objects, arrays, variabeles ##########
 bool blink = LOW;
+
 //RF24
 RF24 radio(7, 8);
 uint8_t address[][6] = {"00001"};
@@ -18,7 +19,7 @@ uint8_t address[][6] = {"00001"};
 //MCP3008 adc1;
 
 //Array für Kanal-Daten
-int inputData[16] = {};
+int inputData[16][2] = {};
 struct ServoData {
   byte sD0=0;
   byte sD1=0;
@@ -41,21 +42,32 @@ struct ServoData {
 ServoData servoData;
 
 //Array für Moving Average Filter
-// #define WINDOW_SIZE 10
-// int mafData[10][5] = {};  //INDEX, VALUE, SUM, READINGS[WINDOW_SIZE], AVERAGED
-
+#define WINDOW_SIZE 10
+int mafData[16][WINDOW_SIZE+4] = {};  // INDEX (0), VALUE (1), SUM (2), AVERAGED (3), READINGS[WINDOW_SIZE] (4-WINDOW_SIZE-1)
+ 
 
 //########## methods ##########
 void readAll(int a) {
   for (int i=0; i<a; i++) {
     adc_select_input(i);
-    inputData[i] = adc_read();
+    inputData[i][0] = adc_read();
   }
 }
 
-void linearMapping() {
-  servoData.sD0 = map(inputData[0], 0, 4095, 0, 180);
-  servoData.sD1 = map(inputData[1], 0, 4095, 0, 180);
+int linearMapping(int a) {
+  int b;
+  return b = map(a, 0, 4095, 40, 140);
+}
+
+void mafFiltering(int a) {
+  mafData[a][2] = mafData[a][2] - mafData[a][mafData[a][0]];       // Remove the oldest entry from the sum
+  mafData[a][mafData[a][0]] = inputData[a][1];                        // Add the newest reading to the window
+  mafData[a][2] = mafData[a][2] + inputData[a][1];                 // Add the newest reading to the sum
+  mafData[a][0] = mafData[a][0]+1;                              // Increment the index, and wrap to 0 if it exceeds the window size
+  if(mafData[a][0] >= WINDOW_SIZE+4) {
+    mafData[a][0] = 4;
+  }
+  mafData[a][3] = mafData[a][2] / WINDOW_SIZE;           // Divide the sum of the window by the window size for the result
 }
 
 //########## setup code ##########
@@ -83,33 +95,37 @@ void setup() {
   radio.setPayloadSize(sizeof(servoData));
   radio.openWritingPipe(address[0]);
   radio.stopListening();
+
+  for(int i=0; i<16; i++) {
+    mafData[i][0] = 4;
+  }
 }
 
 //########## loop code ##########
 void loop() {
   digitalWrite(25, blink);
   blink = !blink;
-//  Serial.println(F("Start the loop!"));
 
   //Daten aus ADC einlesen
   readAll(2);
 
-//  //Moving Average Filter
-//  SUM = SUM - READINGS[INDEX];       // Remove the oldest entry from the sum
-//  READINGS[INDEX] = potValue;           // Add the newest reading to the window
-//  SUM = SUM + potValue;                 // Add the newest reading to the sum
-//  INDEX = (INDEX+1) % WINDOW_SIZE;   // Increment the index, and wrap to 0 if it exceeds the window size
-
-//  AVERAGED = SUM / WINDOW_SIZE;      // Divide the sum of the window by the window size for the result
-  
   //ADC Wert auf PWM Bereich mappen
-  linearMapping();
+  for(int i=0; i<2; i++) {
+    inputData[i][1] = linearMapping(inputData[i][0]);
+  }
+
+  //Moving Average Filter
+  for(int i=0; i<2; i++) {
+    mafFiltering(i);
+  }
+
+  servoData.sD0 = mafData[0][3];
+  servoData.sD1 = mafData[1][3];
   
   radio.write(&servoData, sizeof(servoData));
-//  Serial.println(AVERAGED);
-//  Serial.print("sD0: ");
-//  Serial.print(servoData.sD0);
-//  Serial.print(" sD1: ");
-//  Serial.println(servoData.sD1);
-  delay(10);
+  Serial.print("sD0: ");
+  Serial.print(servoData.sD0);
+  Serial.print(" sD1: ");
+  Serial.println(servoData.sD1);
+//  delay(50);
 }
